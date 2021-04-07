3a80daf696f5e810308e4a36e1938f14c6c7678c
IVProb <jnoojinjr@gmail.com>
2021-04-07 15:07:53 -0500

    set2 IsACharacter DONE

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,15 +6,19 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            //return num1 == num2;
+
+            return (num1 == num2) ? true : false;
+
+            //if(num1 == num2)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
         }
+
 
         public double Subtract(double minuend, double subtrahend)
         {
@@ -28,7 +32,7 @@ namespace ChallengesWithTestsMark8
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            if( number2 > number1)
+            if (number2 > number1)
             {
                 return number1;
             }
